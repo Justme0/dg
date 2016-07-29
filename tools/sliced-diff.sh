1bdc176afc30d921c85205b9e7953ef0b7a85ab1
jiangg <jiangg@mail.ustc.edu.cn>
2016-07-29 10:07:17 +0800

    Prefer vimdiff tool to see difference between original and sliced programs

--- tools/sliced-diff.sh
+++ tools/sliced-diff.sh
@@ -6,7 +6,9 @@ llvm-dis "$M" -o __m.ll
 llvm-dis "${M%%.bc}.sliced" -o __m.sliced.ll
 
 CMD=diff
-if which meld &>/dev/null; then
+if which vimdiff &>/dev/null; then # prefer vimdiff 2016.7.3 jiangg
+	CMD=vimdiff
+elif which meld &>/dev/null; then
 	CMD=meld
 elif which kompare &>/dev/null; then
 	CMD=kompare
